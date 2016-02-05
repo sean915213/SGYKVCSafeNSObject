7f479f1e445671fd8434565018c6965896d02bbd
Sean G Young <syoung@jarustech.com>
2016-02-05 17:43:40 -0500

    Initial project commit.

---
+++ Example/Pods/Target Support Files/Pods-SGYKVCSafeNSObject_Tests/Pods-SGYKVCSafeNSObject_Tests-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double Pods_SGYKVCSafeNSObject_TestsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SGYKVCSafeNSObject_TestsVersionString[];
+
---
+++ Example/Pods/Target Support Files/SGYKVCSafeNSObject/SGYKVCSafeNSObject-umbrella.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+
+FOUNDATION_EXPORT double SGYKVCSafeNSObjectVersionNumber;
+FOUNDATION_EXPORT const unsigned char SGYKVCSafeNSObjectVersionString[];
+
